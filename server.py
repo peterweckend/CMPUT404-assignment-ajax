e2c96078f0020753ee8fa2b9a38b0af823ab8891
PeterWeckend <pweckend@ualberta.ca>
2019-03-01 16:12:14 -0700

    filled out server methods, added some color changing code to the html

--- server.py
+++ server.py
@@ -79,22 +79,25 @@ def hello():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    for key in flask_post_json():
+        myWorld.update(entity, key, flask_post_json()[key])
+    return myWorld.get(entity)
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return myWorld.world()
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return myWorld.get(entity)
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return myWorld.world()
 
 if __name__ == "__main__":
     app.run()
